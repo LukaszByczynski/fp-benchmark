f97417acc7972025cc0ade558b73ed378b7a25b5
Lukasz Byczynski <mail@lukaszbyczynski.com>
2018-10-17 22:29:22 +0200

    Unroll benchmark code

--- src/main/scala/fp/workshop/benchmark/MtlBenchmark.scala
+++ src/main/scala/fp/workshop/benchmark/MtlBenchmark.scala
@@ -7,41 +7,31 @@ class MtlBenchmark {
   @Benchmark
   def mtlStack(): Unit = {
     val codebase = new Codebase
-    1.to(1000).foreach { _ =>
-      codebase.mtlStack.unsafeRunSync()
-    }
+    codebase.mtlStack.unsafeRunSync()
   }
 
   @Benchmark
   def mtlStackOpt(): Unit = {
     val codebase = new Codebase
-    1.to(1000).foreach { _ =>
-      codebase.mtlStackOpt.unsafeRunSync()
-    }
+    codebase.mtlStackOpt.unsafeRunSync()
   }
 
   @Benchmark
   def mtlMeow(): Unit = {
     val codebase = new Codebase
-    1.to(1000).foreach { _ =>
-      codebase.mtlMeow.unsafeRunSync()
-    }
+    codebase.mtlMeow.unsafeRunSync()
   }
 
   @Benchmark
   def imperative(): Unit = {
     val codebase = new Codebase
-    1.to(1000).foreach { _ =>
-      codebase.imperative(Config("", 1), 0, List[String]())
-    }
+    codebase.imperative(Config("", 1), 0, List[String]())
   }
 
   @Benchmark
   def io(): Unit = {
     val codebase = new Codebase
-    1.to(1000).foreach { _ =>
-      codebase.io(Config("", 1), 0, List[String]()).unsafeRunSync()
-    }
+    codebase.io(Config("", 1), 0, List[String]()).unsafeRunSync()
   }
 
 }
